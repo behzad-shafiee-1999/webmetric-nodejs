c4b840e2ec52c29de17948f76784389355626de1
behzad-shafiee-1999 <behzadshafiee1999@yahoo.com>
2024-09-14 14:02:33 +0330

    add: Add pm2 to clustering

--- src/module/user/user.service.ts
+++ src/module/user/user.service.ts
@@ -1,27 +1,25 @@
-import { Injectable } from '@nestjs/common';
-import { RedisService } from '../redis/redis.service';
-import { ConfigService } from '@nestjs/config';
+import { Injectable } from '@nestjs/common'
+import { RedisService } from '../redis/redis.service'
+import { ConfigService } from '@nestjs/config'
 
 @Injectable()
 export class UserService {
-  constructor(
-    private redisService: RedisService,
-    private configService: ConfigService,
-  ) { }
-  async findOne(user_id: string) {
-    let user_info: any = {
-      user_id,
-      firstname: `behzad_${user_id}`,
-      lastname: `shafiee_${user_id}`
+    constructor(
+        private redisService: RedisService,
+        private configService: ConfigService
+    ) {}
+    async findOne(user_id: string) {
+        let user_info: any = {
+            user_id,
+            firstname: `behzad_${user_id}`,
+            lastname: `shafiee_${user_id}`
+        }
+
+        //اگر داده در کچ هست همان را برمیگردانم
+        const result = await this.redisService.get(user_id)
+        if (result) user_info = JSON.parse(result)
+        else await this.redisService.set(user_id, JSON.stringify(user_info), +this.configService.get('REDIS_CACH_TTL'))
+
+        return { user_info }
     }
-
-    //اگر داده در کچ هست همان را برمیگردانم
-    const result = await this.redisService.get(user_id)
-    if (result)
-      user_info = JSON.parse(result)
-    else
-      await this.redisService.set(user_id, JSON.stringify(user_info), +this.configService.get("REDIS_CACH_TTL"))
-
-    return { user_info }
-  }
 }
