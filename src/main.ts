0db1e2a4f8b6f51ce3eebbbf1e36e0d5fa383454
behzad-shafiee-1999 <behzadshafiee1999@yahoo.com>
2024-09-14 13:56:14 +0330

    add: Add pm2 to clustering

--- src/main.ts
+++ src/main.ts
@@ -8,24 +8,23 @@ import { ClusterService } from './module/cluster/cluster.service';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  const config = app.get( ConfigService )
-  const http_port = config.get<string>( 'HTTP_PORT' )
-  const microservice_port = config.get<string>( 'MICROSERVICE_PORT' )
-  const host = config.get<string>( 'HOST' )
+  const config = app.get(ConfigService);
+  const http_port = config.get<string>('HTTP_PORT');
+  const microservice_port = config.get<string>('MICROSERVICE_PORT');
+  const host = config.get<string>('HOST');
 
   const microservice = app.connectMicroservice({
     transport: Transport.TCP,
-    options: { host, port: microservice_port }
+    options: { host, port: microservice_port },
   });
 
-  SwaggerConfig( app )
-  
+  SwaggerConfig(app);
+
   await app.startAllMicroservices();
-  await app.listen(http_port,()=>{
+  await app.listen(http_port, () => {
     console.log(`server: ${host}:${http_port}`);
     console.log(`swagger: ${host}:${http_port}/doc`);
-    
   });
 }
 bootstrap();
-// ClusterService.clusterize(bootstrap);
+ClusterService.clusterize(bootstrap);
