f2bbb16708aa5f9f6ccdad6c64c1948d57a0369e
behzad-shafiee-1999 <behzadshafiee1999@yahoo.com>
2024-09-14 13:58:16 +0330

    add: Add pm2 to clustering

--- src/module/cluster/cluster.service.ts
+++ src/module/cluster/cluster.service.ts
@@ -1,25 +1,24 @@
-const cluster=require ('cluster');
+import cluster from 'cluster';
 import * as os from 'os';
 import { Injectable } from '@nestjs/common';
 
 const numCPUs = os.cpus().length;
-@Injectable()
-export class ClusterService
-{
 
+@Injectable()
+export class ClusterService {
   static clusterize(callback: Function): void {
-    if(cluster.isMaster){
-        console.log(`Master server started on ${process.pid}`);
-        for (let i = 0; i < numCPUs; i++) {
-            cluster.fork();
-        }
-        cluster.on('exit', (worker, code, signal) => {
-            console.log(`Worker ${worker.process.pid} died. Restarting`);
-            cluster.fork();
-        })
+    if (cluster.isPrimary) {
+      console.log(`Master server started on ${process.pid}`);
+      for (let i = 0; i < numCPUs; i++) {
+        cluster.fork();
+      }
+      cluster.on('exit', (worker, code, signal) => {
+        console.log(`Worker ${worker.process.pid} died. Restarting`);
+        cluster.fork();
+      });
     } else {
-        console.log(`Cluster server started on ${process.pid}`)
-        callback();
+      console.log(`Cluster server started on ${process.pid}`);
+      callback();
     }
-}
+  }
 }
